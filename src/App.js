95fd4f45e38738f688a2724870f788646fc5dffc
Tom Nicholas <tom_here@yahoo.com>
2020-04-10 23:57:47 -0700

    add changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React from "react";
-import "./styles.css";
+import React, { useState } from 'react';
+import './styles.css';
 
 export default function App() {
   return (
@@ -10,26 +10,58 @@ export default function App() {
 }
 
 const Calculator = () => {
+  const [calc, setCalc] = useState({
+    value: 0
+  });
+
+  const numberEnter = n => {
+    if (calc.value === 0) {
+      setCalc({ value: n });
+    } else {
+      setCalc({ value: calc.value + '' + n });
+    }
+  };
+
   return (
     <div className="calc-main">
       <div className="calc-top">
-        <div className="calc-value">0</div>
+        <div className="calc-value">{calc.value}</div>
       </div>
       <div className="calc-bottom">
         <div className="calc-left">
           <div className="calc-clear">clear</div>
           <div className="calc-numbers">
-            <div className="calc-number">7</div>
-            <div className="calc-number">8</div>
-            <div className="calc-number">9</div>
-            <div className="calc-number">4</div>
-            <div className="calc-number">5</div>
-            <div className="calc-number">6</div>
-            <div className="calc-number">1</div>
-            <div className="calc-number">2</div>
-            <div className="calc-number">3</div>
-            <div className="calc-number"></div>
-            <div className="calc-number">0</div>
+            <div className="calc-number" onClick={() => numberEnter(7)}>
+              7
+            </div>
+            <div className="calc-number" onClick={() => numberEnter(8)}>
+              8
+            </div>
+            <div className="calc-number" onClick={() => numberEnter(9)}>
+              9
+            </div>
+            <div className="calc-number" onClick={() => numberEnter(4)}>
+              4
+            </div>
+            <div className="calc-number" onClick={() => numberEnter(5)}>
+              5
+            </div>
+            <div className="calc-number" onClick={() => numberEnter(6)}>
+              6
+            </div>
+            <div className="calc-number" onClick={() => numberEnter(1)}>
+              1
+            </div>
+            <div className="calc-number" onClick={() => numberEnter(2)}>
+              2
+            </div>
+            <div className="calc-number" onClick={() => numberEnter(3)}>
+              3
+            </div>
+            <div className="calc-number" />
+            <div className="calc-number" onClick={() => numberEnter(0)}>
+              0
+            </div>
             <div className="calc-number">.</div>
           </div>
         </div>
